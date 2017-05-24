345178356b20efb3a6ea3c44ef95c0cec7a092d7
Robin Aggleton <robin.aggleton@cern.ch>
2017-05-24 11:08:40 +0200

    Add more Dijet control region hists, esp jet3 plots

--- include/QGAnalysisDijetHists.h
+++ include/QGAnalysisDijetHists.h
@@ -17,7 +17,8 @@ public:
 
 protected:
     TH1F *n_jets;
-    TH1F *pt_jet1, *eta_jet1, *phi_jet1, *pt_jet2, *eta_jet2, *phi_jet2, *m_jj;
+    TH1F *pt_jet1, *eta_jet1, *phi_jet1, *pt_jet2, *eta_jet2, *phi_jet2, *m_jj, *pt_jet1_jet2_ratio;
+    TH1F *pt_jet3, *eta_jet3, *pt_jet3_frac;
     TH2F *deta_dphi_jj;
 };
 
--- src/QGAnalysisDijetHists.cxx
+++ src/QGAnalysisDijetHists.cxx
@@ -14,20 +14,29 @@ QGAnalysisDijetHists::QGAnalysisDijetHists(Context & ctx, const string & dirname
   // jets
   n_jets = book<TH1F>("N_jets", "N_{jets}", 10, 0, 10);
 
-  int nbins_pt = 100;
-  int nbins_eta = 40;
+  int nbins_pt = 50;
+  float pt_max = 1000;
+  float eta_max = 5;
+  int nbins_eta = 50;
   int nbins_phi = 60;
-  pt_jet1 = book<TH1F>("pt_jet1", "p_{T}^{jet 1} [GeV/c]", nbins_pt, 0, 200);
-  eta_jet1 = book<TH1F>("eta_jet1", "#eta^{jet 1}", nbins_eta, -2.5, 2.5);
+  pt_jet1 = book<TH1F>("pt_jet1", "p_{T}^{jet 1} [GeV/c]", nbins_pt, 0, pt_max);
+  eta_jet1 = book<TH1F>("eta_jet1", "#eta^{jet 1}", nbins_eta, -eta_max, eta_max);
   phi_jet1 = book<TH1F>("phi_jet1", "#phi^{jet 1}", nbins_phi, -TMath::Pi(), TMath::Pi());
 
-  pt_jet2 = book<TH1F>("pt_jet2", "p_{T}^{jet 2} [GeV/c]", nbins_pt, 0, 200);
-  eta_jet2 = book<TH1F>("eta_jet2", "#eta^{jet 2}", nbins_eta, -2.5, 2.5);
+  pt_jet2 = book<TH1F>("pt_jet2", "p_{T}^{jet 2} [GeV/c]", nbins_pt, 0, pt_max);
+  eta_jet2 = book<TH1F>("eta_jet2", "#eta^{jet 2}", nbins_eta, -eta_max, eta_max);
   phi_jet2 = book<TH1F>("phi_jet2", "#phi^{jet 2}", nbins_phi, -TMath::Pi(), TMath::Pi());
 
-  m_jj = book<TH1F>("m_jj", "m_{jj} [GeV]", 40, 0, 400);
+  pt_jet1_jet2_ratio = book<TH1F>("pt_jet1_jet2_ratio", "p_{T}^{jet 2} / p_{T}^{jet 1}", 20, 0, 1);
+
+  m_jj = book<TH1F>("m_jj", "m_{jj} [GeV]", 50, 0, 2000);
 
   deta_dphi_jj = book<TH2F>("deta_dphi_jj", ";#Delta #eta_{jj};#Delta #phi_{jj}", 60, 0, 6, 60, 0, TMath::Pi());
+
+  // Possible 3rd jet in the event
+  pt_jet3 = book<TH1F>("pt_jet3", "p_{T}^{jet 3} [GeV/c]", nbins_pt, 0, 500);
+  eta_jet3 = book<TH1F>("eta_jet3", "#eta^{jet 3}", nbins_eta, -eta_max, eta_max);
+  pt_jet3_frac = book<TH1F>("pt_jet3_frac", "p_{T}^{jet 3} / #LT p_{T}^{jet 1} + p_{T}^{jet 2} #GT", 20, 0, 1);
 
   // primary vertices
   book<TH1F>("N_pv", "N^{PV}", 50, 0, 50);
@@ -57,11 +66,20 @@ void QGAnalysisDijetHists::fill(const Event & event){
   eta_jet2->Fill(jet2.eta(), weight);
   phi_jet2->Fill(jet2.phi(), weight);
 
+  pt_jet1_jet2_ratio->Fill(jet2.pt() / jet1.pt(), weight);
+
   double mass = (jet1.v4() + jet2.v4()).M();
   m_jj->Fill(mass, weight);
 
   auto diff = jet1.v4() - jet2.v4();
   deta_dphi_jj->Fill(fabs(diff.eta()), fabs(diff.phi()), weight);
+
+  if (Njets >= 3) {
+    Jet jet3 = jets->at(2);
+    pt_jet3->Fill(jet3.pt(), weight);
+    eta_jet3->Fill(jet3.eta(), weight);
+    pt_jet3_frac->Fill(jet3.pt() / (0.5 * (jet1.pt() + jet2.pt())), weight);
+  }
 
   int Npvs = event.pvs->size();
   hist("N_pv")->Fill(Npvs, weight);
