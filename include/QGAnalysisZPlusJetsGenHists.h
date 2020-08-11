d99915dd2620badfe142f39b127cac101c2967f1
Robin Aggleton <robin.aggleton@cern.ch>
2020-08-11 16:04:17 +0200

    Make z+J gen hist lepton choosable

--- include/QGAnalysisZPlusJetsGenHists.h
+++ include/QGAnalysisZPlusJetsGenHists.h
@@ -14,27 +14,27 @@ namespace uhh2examples {
 class QGAnalysisZPlusJetsGenHists: public uhh2::Hists {
 public:
     // use the same constructor arguments as Hists for forwarding:
-    QGAnalysisZPlusJetsGenHists(uhh2::Context & ctx, const std::string & dirname);
+    QGAnalysisZPlusJetsGenHists(uhh2::Context & ctx, const std::string & dirname, const std::string & channel="muon");
 
     virtual void fill(const uhh2::Event & ev) override;
     virtual ~QGAnalysisZPlusJetsGenHists();
-    GenParticle findGenZ(std::vector<GenParticle> & gps);
+    GenParticle findMatrixElementZ(std::vector<GenParticle> & gps);
 
 protected:
-    TH1F *deta_mumu;
-    TH1F *deta_mumu_jet1;
-    TH1F *dphi_mumu;
-    TH1F *dphi_mumu_jet1;
+    TH1F *deta_ll;
+    TH1F *deta_ll_jet1;
+    TH1F *dphi_ll;
+    TH1F *dphi_ll_jet1;
     TH1F *eta_jet1;
     TH1F *eta_jet2;
-    TH1F *eta_mu1;
-    TH1F *eta_mu2;
-    TH1F *eta_mumu;
+    TH1F *eta_lepton1;
+    TH1F *eta_lepton2;
+    TH1F *eta_ll;
     TH1F *eta_z;
     TH1F *gen_ht;
-    TH1F *m_mumu;
+    TH1F *m_ll;
     TH1F *n_jets;
-    TH1F *n_mu;
+    TH1F *n_leptons;
     TH1F *pt_hat;
     TH1F *pt_jet1;
     TH1F *pt_jet1_pt_jet2_ratio;
@@ -43,17 +43,18 @@ protected:
     TH1F *pt_jet2_z_ratio;
     TH1F *pt_jet_genHT_ratio;
     TH1F *pt_hat_pt_jet_ratio;
-    TH1F *pt_mu1;
-    TH1F *pt_mu2;
-    TH1F *pt_mumu;
+    TH1F *pt_lepton1;
+    TH1F *pt_lepton2;
+    TH1F *pt_ll;
     TH1F *pt_z;
     TH1F *q_scale;
     TH1F *jet_kt;
     TH1F *jet_kt_pt_z_ratio;
 
     uhh2::Event::Handle<std::vector<GenJet> > genJets_handle;
-    uhh2::Event::Handle<std::vector<GenParticle>> genMuons_handle;
+    uhh2::Event::Handle<std::vector<GenParticle>> genLeptons_handle;
     uhh2::Event::Handle<double> gen_weight_handle;
+    std::string channel_;
 };
 
 }
--- src/QGAnalysisZPlusJetsGenHists.cxx
+++ src/QGAnalysisZPlusJetsGenHists.cxx
@@ -14,12 +14,16 @@ using namespace std;
 using namespace uhh2;
 using namespace uhh2examples;
 
-QGAnalysisZPlusJetsGenHists::QGAnalysisZPlusJetsGenHists(Context & ctx, const string & dirname):
-Hists(ctx, dirname)
+QGAnalysisZPlusJetsGenHists::QGAnalysisZPlusJetsGenHists(Context & ctx, const string & dirname, const string & channel):
+Hists(ctx, dirname),
+channel_(channel)
 {
   if (ctx.get("dataset_type") == "MC") {
     genJets_handle = ctx.get_handle< std::vector<GenJet> > ("GoodGenJets");
-    genMuons_handle = ctx.get_handle<std::vector<GenParticle>>("GoodGenMuons");
+    if (channel_ != "muon" && channel_ != "electron")
+      throw runtime_error("QGAnalysisZPlusJetsGenHists: channel should be muon or electron");
+
+    genLeptons_handle = ctx.get_handle<std::vector<GenParticle>>(channel_ == "muon" ? "GoodGenMuons" : "GoodGenElectrons");
     gen_weight_handle = ctx.get_handle<double>("gen_weight");
 
     int nbins_pt = 500;
@@ -31,27 +35,28 @@ Hists(ctx, dirname)
     int nbins_phi = 128;
     float phi_max = 3.2;
 
-    float mu_pt_max = 1000;
+    float lepton_pt_max = 1000;
 
     // TString zName = "Z";
-    TString zName = "#mu#mu";
+    TString lName = (channel_ == "muon") ? "#mu" : "e";
+    TString zName = lName + lName;
     TString binByVar = "pt_z";
 
-    deta_mumu = book<TH1F>("deta_mumu", TString::Format(";|#Deltay_{%s}|;", zName.Data()), nbins_eta, 0, 2*eta_max);
-    deta_mumu_jet1 = book<TH1F>("deta_mumu_jet1", TString::Format(";|#Deltay_{%s, genjet 1}|;", zName.Data()), nbins_eta, 0, 2*eta_max);
-    dphi_mumu = book<TH1F>("dphi_mumu", TString::Format(";|#Delta#phi_{%s}|;", zName.Data()), nbins_phi, 0, phi_max);
-    dphi_mumu_jet1 = book<TH1F>("dphi_mumu_jet1", TString::Format(";|#Delta#phi_{%s, genjet 1}|;", zName.Data()), nbins_phi, 0, phi_max);
+    deta_ll = book<TH1F>("deta_ll", TString::Format(";|#Deltay_{%s}|;", zName.Data()), nbins_eta, 0, 2*eta_max);
+    deta_ll_jet1 = book<TH1F>("deta_ll_jet1", TString::Format(";|#Deltay_{%s, genjet 1}|;", zName.Data()), nbins_eta, 0, 2*eta_max);
+    dphi_ll = book<TH1F>("dphi_ll", TString::Format(";|#Delta#phi_{%s}|;", zName.Data()), nbins_phi, 0, phi_max);
+    dphi_ll_jet1 = book<TH1F>("dphi_ll_jet1", TString::Format(";|#Delta#phi_{%s, genjet 1}|;", zName.Data()), nbins_phi, 0, phi_max);
     eta_jet1 = book<TH1F>("eta_jet1", ";y^{genjet 1};", nbins_eta, -eta_max, eta_max);
     eta_jet2 = book<TH1F>("eta_jet2", ";y^{genjet 2};", nbins_eta, -eta_max, eta_max);
-    eta_mu1 = book<TH1F>("eta_mu1", ";y^{#mu1};", nbins_eta, -eta_max, eta_max);
-    eta_mu2 = book<TH1F>("eta_mu2", ";y^{#mu2};", nbins_eta, -eta_max, eta_max);
-    eta_mumu = book<TH1F>("eta_mumu", TString::Format(";y^{%s};", zName.Data()), nbins_eta, -eta_max, eta_max);
+    eta_lepton1 = book<TH1F>("eta_lepton1", TString::Format(";y^{%s1};", lName.Data()), nbins_eta, -eta_max, eta_max);
+    eta_lepton2 = book<TH1F>("eta_lepton2", TString::Format(";y^{%s2};", lName.Data()), nbins_eta, -eta_max, eta_max);
+    eta_ll = book<TH1F>("eta_ll", TString::Format(";y^{%s};", zName.Data()), nbins_eta, -eta_max, eta_max);
     // eta_z = book<TH1F>("eta_z", ";y^{Z};", nbins_eta, -eta_max, eta_max);
     gen_ht = book<TH1F>("gen_ht", ";H_{T}^{Gen} [GeV];", 500, 0, 5000);
     pt_hat = book<TH1F>("ptHat", ";#hat{p}_{T} [GeV];", 2500, 0, 5000);
-    m_mumu = book<TH1F>("m_mumu", TString::Format(";m_{%s} [GeV];", zName.Data()), 80, 90-40, 90+40);
+    m_ll = book<TH1F>("m_ll", TString::Format(";m_{%s} [GeV];", zName.Data()), 80, 90-40, 90+40);
     n_jets = book<TH1F>("n_jets", ";N_{genjets};", 10, 0, 10);
-    n_mu = book<TH1F>("n_mu", ";N_{#mu};", 10, 0, 10);
+    n_leptons = book<TH1F>("n_leptons", TString::Format(";N_{%s};", lName.Data()), 10, 0, 10);
     pt_jet1 = book<TH1F>("pt_jet1", ";p_{T}^{genjet 1} [GeV];", nbins_pt, 0, pt_max);
     pt_jet1_pt_jet2_ratio = book<TH1F>("pt_jet1_pt_jet2_ratio", ";p_{T}^{genjet 1} / p_{T}^{genjet 2};", 200, 0, 10);
     pt_jet1_z_ratio = book<TH1F>("pt_jet1_z_ratio", TString::Format(";p_{T}^{genjet 1}/ p_{T}^{%s};", zName.Data()), 50, 0, 5);
@@ -59,12 +64,12 @@ Hists(ctx, dirname)
     pt_jet2_z_ratio = book<TH1F>("pt_jet2_z_ratio", TString::Format(";p_{T}^{genjet 2} / p_{T}^{%s};", zName.Data()), 60, 0, 3);
     pt_jet_genHT_ratio = book<TH1F>("pt_jet_genHT_ratio", ";p_{T}^{genjet 1}/GenHT;", 250, 0, 2.5);
     pt_hat_pt_jet_ratio = book<TH1F>("pt_hat_pt_jet_ratio", ";p_{T}^{genjet 1}/#hat{p}_{T};", 500, 0, 10);
-    pt_mu1 = book<TH1F>("pt_mu1", ";p_{T}^{#mu1};", nbins_pt, 0, mu_pt_max);
-    pt_mu2 = book<TH1F>("pt_mu2", ";p_{T}^{#mu2};", nbins_pt, 0, mu_pt_max);
-    pt_mumu = book<TH1F>("pt_mumu", TString::Format(";p_{T}^{%s} [GeV];", zName.Data()), nbins_pt, 0, mu_pt_max);
-    pt_z = book<TH1F>("pt_z", ";p_{T}^{Z} [GeV];", nbins_pt, 0, mu_pt_max);
+    pt_lepton1 = book<TH1F>("pt_lepton1", TString::Format(";p_{T}^{%s1};", lName.Data()), nbins_pt, 0, lepton_pt_max);
+    pt_lepton2 = book<TH1F>("pt_lepton2", TString::Format(";p_{T}^{%s2};", lName.Data()), nbins_pt, 0, lepton_pt_max);
+    pt_ll = book<TH1F>("pt_ll", TString::Format(";p_{T}^{%s} [GeV];", zName.Data()), nbins_pt, 0, lepton_pt_max);
+    pt_z = book<TH1F>("pt_z", ";p_{T}^{Z} [GeV];", nbins_pt, 0, lepton_pt_max);
     q_scale = book<TH1F>("q_scale", ";q scale [GeV];", 250, 0, 500);
-    jet_kt = book<TH1F>("jet_kt", ";Parton k_{T} [GeV];", nbins_pt, 0, mu_pt_max);
+    jet_kt = book<TH1F>("jet_kt", ";Parton k_{T} [GeV];", nbins_pt, 0, lepton_pt_max);
     jet_kt_pt_z_ratio = book<TH1F>("jet_kt_pt_z_ratio", ";Parton k_{T} / p_{T}^{Z};", 250, 0, 2.5);
   }
 
@@ -79,12 +84,12 @@ void QGAnalysisZPlusJetsGenHists::fill(const Event & event){
   int Njets = genjets->size();
   n_jets->Fill(Njets, weight);
 
-  const std::vector<GenParticle> * genmuons = &event.get(genMuons_handle);
-  int Nmuons = genmuons->size();
-  n_mu->Fill(Nmuons, weight);
+  const std::vector<GenParticle> * genleptons = &event.get(genLeptons_handle);
+  int Nleptons = genleptons->size();
+  n_leptons->Fill(Nleptons, weight);
 
-  // Don't use - Z probably doesn't exist in list of GPs
-  GenParticle genZ = findGenZ(*event.genparticles);
+  // This is the Z made from ME leptons, not final state ones
+  GenParticle genZ = findMatrixElementZ(*event.genparticles);
   // cout << genZ.pdgId() << " : " << genZ.status() << " : " << genZ.pt() << endl;
   pt_z->Fill(genZ.pt(), weight);
   // eta_z->Fill(genZ.Rapidity(), weight);
@@ -104,24 +109,24 @@ void QGAnalysisZPlusJetsGenHists::fill(const Event & event){
   float qScale = event.genInfo->qScale();
   q_scale->Fill(qScale, weight);
 
-  if (Nmuons < 2) return;
-  GenParticle mu1 = genmuons->at(0);
-  GenParticle mu2 = genmuons->at(1);
-  LorentzVector z_cand = mu1.v4() + mu2.v4();
+  if (Nleptons < 2) return;
+  GenParticle l1 = genleptons->at(0);
+  GenParticle l2 = genleptons->at(1);
+  LorentzVector z_cand = l1.v4() + l2.v4();
   float z_pt = z_cand.pt();
-  pt_mumu->Fill(z_pt, weight);
-  eta_mumu->Fill(z_cand.Rapidity(), weight);
+  pt_ll->Fill(z_pt, weight);
+  eta_ll->Fill(z_cand.Rapidity(), weight);
 
-  pt_mu1->Fill(mu1.pt(), weight);
-  eta_mu1->Fill(mu1.Rapidity(), weight);
+  pt_lepton1->Fill(l1.pt(), weight);
+  eta_lepton1->Fill(l1.Rapidity(), weight);
 
-  pt_mu2->Fill(mu2.pt(), weight);
-  eta_mu2->Fill(mu2.Rapidity(), weight);
+  pt_lepton2->Fill(l2.pt(), weight);
+  eta_lepton2->Fill(l2.Rapidity(), weight);
 
-  m_mumu->Fill(z_cand.M(), weight);
+  m_ll->Fill(z_cand.M(), weight);
 
-  deta_mumu->Fill(fabs(mu1.Rapidity() - mu2.Rapidity()), weight);
-  dphi_mumu->Fill(fabs(deltaPhi(mu1, mu2)), weight);
+  deta_ll->Fill(fabs(l1.Rapidity() - l2.Rapidity()), weight);
+  dphi_ll->Fill(fabs(deltaPhi(l1, l2)), weight);
 
   // Jets
   if (Njets < 1) return;
@@ -140,8 +145,8 @@ void QGAnalysisZPlusJetsGenHists::fill(const Event & event){
   pt_jet_genHT_ratio->Fill(jet1_pt / genHT, weight);
 
   pt_jet1_z_ratio->Fill(jet1_pt / z_pt, weight);
-  deta_mumu_jet1->Fill(fabs(z_cand.Rapidity() - jet1.Rapidity()), weight);
-  dphi_mumu_jet1->Fill(fabs(deltaPhi(jet1, z_cand)), weight);
+  deta_ll_jet1->Fill(fabs(z_cand.Rapidity() - jet1.Rapidity()), weight);
+  dphi_ll_jet1->Fill(fabs(deltaPhi(jet1, z_cand)), weight);
 
   if (Njets >= 2) {
     auto jet2 = genjets->at(1);
@@ -153,13 +158,19 @@ void QGAnalysisZPlusJetsGenHists::fill(const Event & event){
 
 }
 
+/**
+ * Find Matrix Element "Z" by reconstructing dileptons
+ * Note that due to v.occasional tau decays, and how the decay tree gets smushed,
+ * we just look for those PDGIDs explicitly.
+ */
 // this should probably be in the main MC module and set a handle
-GenParticle QGAnalysisZPlusJetsGenHists::findGenZ(std::vector<GenParticle> & gps) {
-  // note that we can't check PDGID - it could be a photon instead
+GenParticle QGAnalysisZPlusJetsGenHists::findMatrixElementZ(std::vector<GenParticle> & gps) {
+
   bool foundFirstLepton = false;
   GenParticle firstLepton, secondLepton;
+  auto targetPDGID = (channel_ == "muon") ? PDGID::MUON : PDGID::ELECTRON;
   for (const auto & itr : gps) {
-    if (abs(itr.pdgId()) >= PDGID::ELECTRON && abs(itr.pdgId()) <= PDGID::TAU_NEUTRINO) {
+    if (abs(itr.pdgId()) == targetPDGID) {
       if (!foundFirstLepton) {
         foundFirstLepton = true;
         firstLepton.set_pdgId(itr.pdgId());
