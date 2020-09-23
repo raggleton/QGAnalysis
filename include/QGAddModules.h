3d8775eb718200b672db07509fee9569b219437d
Robin Aggleton <robin.aggleton@cern.ch>
2020-09-23 22:00:19 +0200

    Add new binning derived on 2016v2 samples in 102X, pt > 0 (multiplicity pt > 1), fix lambda jet radius bug
    Derived on
    workdir_102X_v3data_v2mc_ak4puppi_fixSelCutOrder_puppiJER_tightJetId_constitPt0MultPt1_WeightCuts_fixLambda
    
    Uses SoftDrop instead of MMDT
    
    Uses groomed jets for charged-only variables,
    since ungroomed binning has a large drop in purity/stability
    when applied to groomed jets.

--- include/QGAddModules.h
+++ include/QGAddModules.h
@@ -763,7 +763,8 @@ namespace Binning {
     // 0.0, 0.16, 0.23, 0.3, 0.36, 0.42, 0.49, 0.56, 0.63, 0.72, 1.0 // target 0.5, across both dijets
     // 0.0, 0.19, 0.29, 0.38, 0.49, 0.6, 0.75, 1.0 // target 0.6, forward
     // 0.0, 0.14, 0.22, 0.29, 0.35, 0.42, 0.49, 0.56, 0.64, 1.0 //target 0.5, cen+fwd
-    0.0, 0.14, 0.21, 0.28, 0.34, 0.4, 0.47, 0.54, 0.61, 1.0 //target 0.5, cen+fwd, AK axis
+    // 0.0, 0.14, 0.21, 0.28, 0.34, 0.4, 0.47, 0.54, 0.61, 1.0 //target 0.5, cen+fwd, AK axis
+    0.0, 0.17, 0.25, 0.32, 0.38, 0.45, 0.52, 0.59, 0.66, 1.0 // target 0.5, cen+fwd, WTA axis, pt > 0, ungroomed, fixLambda, >= 2 constits
   };
   const int nbins_lha_gen(lha_bin_edges_gen.size() - 1);
 
@@ -776,7 +777,8 @@ namespace Binning {
     // 0.0, 0.09, 0.14, 0.19, 0.24, 0.3, 0.36, 0.43, 0.51, 0.6, 0.71, 1.0 // target 0.5, across both dijets
     // 0.0, 0.11, 0.17, 0.24, 0.32, 0.42, 0.54, 0.69, 1.0 // target 0.6, forward
     // 0.0, 0.05, 0.09, 0.12, 0.15, 0.18, 0.22, 0.26, 0.31, 0.36, 0.42, 0.49, 0.56, 0.64, 1.0 // target 0.5, cen+fwd
-    0.0, 0.05, 0.09, 0.14, 0.19, 0.25, 0.32, 0.39, 0.47, 0.55, 0.63, 0.73, 1.0 // target 0.5, cen+fwd groomed (as coarser), AK axis
+    // 0.0, 0.05, 0.09, 0.14, 0.19, 0.25, 0.32, 0.39, 0.47, 0.55, 0.63, 0.73, 1.0 // target 0.5, cen+fwd groomed (as coarser), AK axis
+    0.0, 0.05, 0.09, 0.14, 0.19, 0.25, 0.32, 0.39, 0.47, 0.55, 0.63, 0.72, 1.0 // target 0.5, cen+fwd groomed (to avoid big drop for ungroomed), WTA axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_lha_charged_gen(lha_charged_bin_edges_gen.size() - 1);
 
@@ -789,7 +791,8 @@ namespace Binning {
     // 0, 9, 15, 22, 35, 50, 75, 100, 150 // target 0.5, across both dijets
     // 0.0, 11.0, 18.0, 25.0, 150.0 // target 0.6, central
     // 0.0, 10, 15, 20, 27, 50, 75, 100, 150 // target 0.5, cen+fwd
-    0.0, 10, 15, 20, 30, 50, 75, 100, 150 // target 0.5, cen+fwd, AK axis. 30, 50, 75, 100 added by hand otherwise no granulairty
+    // 0.0, 10, 15, 20, 30, 50, 75, 100, 150 // target 0.5, cen+fwd, AK axis. 30, 50, 75, 100 added by hand otherwise no granulairty
+    0, 10, 15, 20, 30, 50, 75, 100, 150 // based on target 0.5, cen+fwd, but rounded to nearest multiple of 5. 30, 50, 75, 100 added by hand otherwise no granulairty, >= 2 constits
   };
   const int nbins_puppiMultiplicity_gen(puppiMultiplicity_bin_edges_gen.size() - 1);
 
@@ -802,7 +805,8 @@ namespace Binning {
     // 0, 9, 15, 22, 35, 50, 75, 100, 150 // target 0.5, across both dijets
     // 0.0, 4.0, 6.0, 9.0, 12.0, 16.0, 23.0, 150.0 // target 0.6, forward
     // 0.0, 2.0, 3.0, 4.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 18.0, 21.0, 25.0, 32.0, 91, 150.0 // target 0.5, cen+fwd, 91 added as halfway between 32 and 150
-   0.0, 3.0, 5.0, 7.0, 10.0, 13.0, 16.0, 20.0, 30, 50, 75, 100, 150.0 // target 0.5, cen+fwd groomed, AK axis, added 30, 50, 75, 100 manually
+   // 0.0, 3.0, 5.0, 8.0, 11.0, 14.0, 18.0, 22.0, 26.0, 31.0, 37.0, 45.0, 150.0 // target 0.5, cen+fwd groomed, AK axis, added 30, 50, 75, 100 manually
+   2, 5.0, 8.0, 11.0, 14.0, 18.0, 22.0, 26.0, 31.0, 37.0, 45.0, 150.0 // target 0.5, cen+fwd groomed, AK axis, pt > 1, start at 2 since >=2 constits required.
   };
   const int nbins_puppiMultiplicity_charged_gen(puppiMultiplicity_charged_bin_edges_gen.size() - 1);
 
@@ -815,7 +819,8 @@ namespace Binning {
     // 0.0, 0.09, 0.14, 0.25, 1.0 // target 0.5, across both dijets
     // 0.0, 0.1, 0.17, 0.4, 1.0 // target 0.6, forward
     // 0.0, 0.07, 0.1, 0.15, 0.24, 0.45, 1.0 // target 0.5, cen+fwd
-    0.0, 0.07, 0.1, 0.15, 0.24, 1.0 // target 0.5, cen+fwd, AK axis
+    // 0.0, 0.07, 0.1, 0.15, 0.24, 1.0 // target 0.5, cen+fwd, AK axis
+    0.0, 0.06, 0.09, 0.13, 0.19, 0.3, 1.0 // target 0.5, cen+fwd, WTA axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_pTD_gen(pTD_bin_edges_gen.size() - 1);
 
@@ -828,7 +833,8 @@ namespace Binning {
     // 0.0, 0.09, 0.12, 0.15, 0.19, 0.24, 0.31, 0.4, 0.53, 0.73, 1.0 // target 0.5, across both dijets
     // 0.0, 0.09, 0.12, 0.16, 0.21, 0.29, 0.41, 0.6, 1.0 // target 0.6, forward
     // 0.0, 0.08, 0.1, 0.12, 0.14, 0.17, 0.2, 0.24, 0.29, 0.34, 0.4, 0.47, 0.55, 0.65, 0.76, 0.89, 1.0 // target 0.5, cen+fwd
-    0.0, 0.07, 0.09, 0.11, 0.14, 0.18, 0.23, 0.3, 0.39, 0.51, 0.64, 1.0 // target 0.5, cen+fwd groomed (as ungroomed had too large drop in purity/stab), AK axis
+    // 0.0, 0.07, 0.09, 0.11, 0.14, 0.18, 0.23, 0.3, 0.39, 0.51, 0.64, 1.0 // target 0.5, cen+fwd groomed (as ungroomed had too large drop in purity/stab), AK axis
+    0.0, 0.06, 0.08, 0.1, 0.13, 0.17, 0.22, 0.28, 0.36, 0.47, 0.59, 0.73, 1.0 // target 0.5, cen+fwd groomed (as ungroomed had too large drop in purity/stab), pt > 0, >= 2 constits
   };
   const int nbins_pTD_charged_gen(pTD_charged_bin_edges_gen.size() - 1);
 
@@ -841,7 +847,8 @@ namespace Binning {
     // 0.0, 0.04, 0.07, 0.12, 0.19, 0.27, 0.37, 0.52, 1.0 // target 0.5, across both dijets
     // 0.0, 0.06, 0.16, 0.3, 0.51, 1.0 // target 0.6, forward
     // 0.0, 0.04, 0.08, 0.145, 0.225, 0.32, 0.445, 1.0 // target 0.5, cen+fwd
-    0.0, 0.04, 0.08, 0.14, 0.195, 0.25, 1.0 // target 0.5, cen+fwd, AK axis
+    // 0.0, 0.04, 0.08, 0.14, 0.195, 0.25, 1.0 // target 0.5, cen+fwd, AK axis
+    0.0, 0.05, 0.09, 0.15, 0.205, 0.26, 1.0 // target 0.5, cen+fwd, antiKT axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_thrust_gen(thrust_bin_edges_gen.size() - 1);
 
@@ -854,7 +861,8 @@ namespace Binning {
     // 0.0, 0.02, 0.04, 0.07, 0.11, 0.17, 0.25, 0.36, 0.52, 1.0 // target 0.5, across both dijets
     // 0.0, 0.01, 0.02, 0.04, 0.07, 0.11, 0.18, 0.29, 0.48, 1.0 // target 0.6, forward
     // 0.0, 0.005, 0.01, 0.015, 0.025, 0.035, 0.05, 0.065, 0.085, 0.115, 0.15, 0.2, 0.265, 0.355, 0.48, 0.725, 1.0 // target 0.5, cen+fwd
-    0.0, 0.005, 0.015, 0.03, 0.06, 0.105, 0.16, 0.23, 0.315, 0.425, 1.0 // target 0.5, cen+fwd groomed, AK axis
+    // 0.0, 0.005, 0.015, 0.03, 0.06, 0.105, 0.16, 0.23, 0.315, 0.425, 1.0 // target 0.5, cen+fwd groomed, AK axis
+    0.0, 0.005, 0.015, 0.03, 0.055, 0.09, 0.125, 0.16, 0.195, 0.23, 0.27, 0.315, 0.375, 1.0 // target 0.5, cen+fwd groomed, antiKT axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_thrust_charged_gen(thrust_charged_bin_edges_gen.size() - 1);
 
@@ -867,7 +875,8 @@ namespace Binning {
     // 0.0, 0.08, 0.13, 0.18, 0.24, 0.31, 0.39, 0.47, 0.58, 1.0 // target 0.5, across both dijets
     // 0.0, 0.12, 0.2, 0.32, 0.45, 0.64, 1.0 // target 0.6, forward
     // 0.0, 0.09, 0.145, 0.205, 0.28, 0.36, 0.445, 0.545, 1.0 // target 0.5, cen+fwd
-    0.0, 0.09, 0.145, 0.205, 0.28, 0.355, 0.43, 0.515, 1.0 // target 0.5, cen+fwd, AK axis
+    // 0.0, 0.09, 0.145, 0.205, 0.28, 0.355, 0.43, 0.515, 1.0 // target 0.5, cen+fwd, AK axis
+    0.0, 0.105, 0.165, 0.23, 0.305, 0.38, 0.46, 0.55, 1.0 // target 0.5, cen+fwd, WTA axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_width_gen(width_bin_edges_gen.size() - 1);
 
@@ -880,7 +889,8 @@ namespace Binning {
     // 0.0, 0.04, 0.07, 0.1, 0.14, 0.18, 0.23, 0.29, 0.37, 0.46, 0.58, 0.99, 1.0 // target 0.5, across both dijets
     // 0.0, 0.04, 0.07, 0.11, 0.16, 0.22, 0.3, 0.41, 0.57, 1.0 // target 0.6, forward
     // 0.0, 0.015, 0.025, 0.035, 0.05, 0.065, 0.085, 0.105, 0.13, 0.16, 0.195, 0.235, 0.28, 0.335, 0.4, 0.48, 0.585, 1.0 // target 0.5, cen+fwd
-    0.0, 0.01, 0.025, 0.045, 0.075, 0.11, 0.155, 0.21, 0.275, 0.34, 0.415, 0.495, 0.595, 1.0 // target 0.5, cen+fwd groomed, AK axis
+    // 0.0, 0.01, 0.025, 0.045, 0.075, 0.11, 0.155, 0.21, 0.275, 0.34, 0.415, 0.495, 0.595, 1.0 // target 0.5, cen+fwd groomed, AK axis
+    0.0, 0.01, 0.025, 0.045, 0.075, 0.11, 0.155, 0.21, 0.275, 0.34, 0.41, 0.485, 0.575, 1.0 // target 0.5, cen+fwd groomed, WTA axis, pt > 0, fixLambda, >= 2 constits
   };
   const int nbins_width_charged_gen(width_charged_bin_edges_gen.size() - 1);
 
